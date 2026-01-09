6f0c023721bf6b65b51fb13de6ba41cf759dbf41
Chirath <chirathtimodh@gmail.com>
2026-01-10 04:19:16 +0530

    fix: db init

--- database/init.sql
+++ database/init.sql
@@ -1,14 +1,7 @@
 CREATE DATABASE FlowMeter;
-GO
-
-USE FlowMeter;
-GO
 
 CREATE LOGIN FlowMeter WITH PASSWORD = 'passwordhere';
-GO
 
 CREATE USER FlowMeter FOR LOGIN FlowMeter;
-GO
 
 ALTER ROLE db_owner ADD MEMBER FlowMeter;
-GO
